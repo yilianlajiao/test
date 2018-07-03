a9b515f3942ae297d9a8b7171595233813221e9e
yilianlajiao <lele5533616@sina.com>
2018-07-03 16:57:59 +0800

    测试使用的哦哦哦哦哦

--- src/main/java/lianwei/test/App.java
+++ src/main/java/lianwei/test/App.java
@@ -2,10 +2,9 @@ package lianwei.test;
 
 /**
  * Hello world!
- *
  */
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello W111orld!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello W111111111orld!");
+    }
 }
