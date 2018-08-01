3a76f08c272ad168f70ee7d7772d2279f38f1db3
yilianlajiao <lele5533616@sina.com>
2018-08-01 11:41:55 +0800

    1111

--- src/main/java/lianwei/test/App.java
+++ src/main/java/lianwei/test/App.java
@@ -5,7 +5,6 @@ package lianwei.test;
  */
 public class App {
     public static void main(String[] args) {
-    	System.err.println(11111111111);
         System.out.println("Hello W111111werwre111去微软认为111orld!");
     }
 }
