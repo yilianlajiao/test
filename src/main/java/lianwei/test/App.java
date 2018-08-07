7fce73e8b1504bf939bb8659a652b9be08f0c7d2
yilianlajiao <lele5533616@sina.com>
2018-08-07 16:53:42 +0800

    44555

--- src/main/java/lianwei/test/App.java
+++ src/main/java/lianwei/test/App.java
@@ -11,6 +11,7 @@ public class App {
     	System.err.println(44444444);
     	System.err.println(55555555);
     	System.err.println(6777777);
+    	System.err.println(88888888);
         System.out.println("Hello W111111werwre111去微软认为111orld!");
     }
 }
