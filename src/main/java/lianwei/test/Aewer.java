ffa4fd44ae03ff0d7f0dc51b559712d37069d312
yilianlajiao <lele5533616@sina.com>
2018-08-27 14:08:30 +0800

    1111

---
+++ src/main/java/lianwei/test/Aewer.java
@@ -0,0 +1,17 @@
+package lianwei.test;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+    	System.err.println(1111111);
+    	System.err.println(33333);
+    	System.err.println(2222222);
+    	System.err.println(44444444);
+    	System.err.println(55555555);
+    	System.err.println(6777777);
+    	System.err.println(88888888);
+        System.out.println("Hello W111111werwre111去微软认为111orld!");
+    }
+}
